6db7167b33b6860d692b7478e329dd28398395f9
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-06-28 18:06:15 -0600

    Declaración y asignación de variables

--- app/src/main/java/com/example/arciniega/proyecto05/MainActivity.java
+++ app/src/main/java/com/example/arciniega/proyecto05/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.arciniega.proyecto05;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaramos un arreglo con los valores que se mostaran en el Spinner
+    private String[] spinnerDatos = new String[]{
+            "Robert De Niro",
+            "Chicarcas A Secas",
+            "Nicole Kidman De Gonzalez",
+            "Victor Balderrama",
+            "Simone Simons De Barroso",
+            "Brad Pitt"
+    };
+
+    // Declaramos los elementos Java que se relacionarán con los elementos del XML
+    private Spinner spinner;
+    private TextView mensaje;
+
+    // Se crea un adaptador de datos para el control spinner
+    // El adaptador recibe tres parametros: this - Contexto de la aplicacion, R.layout... - Indicando que soportada un dropdown - spinnerDatos - El arreglo de datos
+    private ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,spinnerDatos);;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Se relacionan los elementos Java con los elementos del XML
+        spinner = (Spinner)findViewById(R.id.spinner);
+        mensaje = (TextView)findViewById(R.id.txtMensaje);
+
+        // Enlazar el adaptador con el spinner
+        spinner.setAdapter(adaptador);
+
+        // Establecer un listener para el evento 'Selección de item'
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mensaje.setText("Hola " + spinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+
+            }
+        });
     }
 }
